1df6703491b7a3a3f8598ec500c5da39ee836ebb
Alexis Torres Valdes <alexis89.dev@gmail.com>
2021-05-03 11:23:09 -0400

    Modificado el manual de usuario

--- usbsecurity_server/__version__.py
+++ usbsecurity_server/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'usbsecurity-server'
 __description__ = 'usbsecurity-server is the server program to control access to USB ports'
 __url__ = 'https://pypi.org/project/usbsecurity-server/'
-__version__ = '2.0.7'
+__version__ = '2.0.8'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
