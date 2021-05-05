3b2ab25b81d6daee1c46cd17ef458e296668b3ae
Alexis Torres Valdes <alexis89.dev@gmail.com>
2021-05-04 22:01:24 -0400

    Agregado copyright en todos los modulos

--- usbsecurity_server/__version__.py
+++ usbsecurity_server/__version__.py
@@ -1,7 +1,25 @@
+#  This module belongs to the usbsecurity-server project.
+#  Copyright (c) 2021 Alexis Torres Valdes
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  Contact: alexis89.dev@gmail.com
+
 __title__ = 'usbsecurity-server'
 __description__ = 'usbsecurity-server is the server program to control access to USB ports'
 __url__ = 'https://pypi.org/project/usbsecurity-server/'
-__version__ = '4.2.0'
+__version__ = '4.3.0'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
