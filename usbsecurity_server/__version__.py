a6d09573a715d771f36c23010b345e463e619318
Alexis Torres Valdes <alexis89.dev@gmail.com>
2021-05-10 13:46:05 -0400

    Nueva rama 'develop'

--- usbsecurity_server/__version__.py
+++ usbsecurity_server/__version__.py
@@ -19,7 +19,7 @@
 __title__ = 'usbsecurity-server'
 __description__ = 'usbsecurity-server is the server program to control access to USB ports'
 __url__ = 'https://pypi.org/project/usbsecurity-server/'
-__version__ = '4.9.1'
+__version__ = '5.0.0'
 __author__ = 'Alexis Torres Valdes'
 __author_email__ = 'alexis89.dev@gmail.com'
 __credits__ = ['Alexis Torres Valdes']
